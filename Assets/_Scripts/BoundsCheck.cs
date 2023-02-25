6192cd63bc1300033a7c852eb5963f8eb9c635ec
Junyaoooo <123599025+Junyaoooo@users.noreply.github.com>
2023-02-24 21:15:35 -0500

    enemy destory

--- Assets/_Scripts/BoundsCheck.cs
+++ Assets/_Scripts/BoundsCheck.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BoundsCheck : MonoBehaviour
 {
+    public enum eScreenLocs 
+    {
+        onscreen=0,
+        offRight=1,
+        offLeft=2,
+        offUp=3,
+        offDown=8,
+        
+    }
+
 
     public enum eType 
     {
@@ -14,12 +25,21 @@ public class BoundsCheck : MonoBehaviour
     public float radius = 1.1f;
     public float cameraHeight;
     public float cameraWidth;
+    public bool keepOnScreen = true;
+    //public bool isOnScreen = true;
+    public eScreenLocs screenLocs=eScreenLocs.onscreen;
     // Start is called before the first frame update
     void Awake()
     {
         cameraHeight = Camera.main.orthographicSize;
         cameraWidth = Camera.main.aspect * cameraHeight;
     }
+
+    public bool isOnScreen 
+    {
+        get { return (screenLocs == eScreenLocs.onscreen); }
+    }
+
 
     // Update is called once per frame
     void LateUpdate()
@@ -37,28 +57,43 @@ public class BoundsCheck : MonoBehaviour
         }
 
         Vector3 popo = transform.position;
+        screenLocs = eScreenLocs.onscreen;
+       // isOnScreen = true;
 
         if (popo.x > cameraWidth+ checkRadius) 
         {
             popo.x=cameraWidth+ checkRadius;
+            screenLocs |= eScreenLocs.offRight;
+           // isOnScreen = false;
         }
         if (popo.x < -cameraWidth- checkRadius) 
         {
             popo.x=-cameraWidth- checkRadius;
+            screenLocs |= eScreenLocs.offLeft;
+            // isOnScreen = false;
         }
 
         if (popo.y > cameraHeight+ checkRadius) 
         {
             popo.y=cameraHeight+ checkRadius;
+            screenLocs |= eScreenLocs.offUp;
+            // isOnScreen = false;
         }
 
-        if (popo.y <-cameraHeight- checkRadius) 
+        if (popo.y <-cameraHeight- checkRadius)
         {
-            popo.y = -cameraHeight- checkRadius;
+            popo.y = -cameraHeight - checkRadius;
+            screenLocs |= eScreenLocs.offDown;
+            // isOnScreen = false;
         }
 
-
-        transform.position = popo;
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = popo;
+            screenLocs= eScreenLocs.onscreen;
+            // isOnScreen= true;
+        }
+        //transform.position = popo;
 
     }
 }
---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 15f;
+    public float fireRate = 0.33f;
+    public float health = 10;
+    public int score = 100;
+
+
+    private BoundsCheck bndCheck;
+
+    void Awake() 
+    {
+        bndCheck=GetComponent<BoundsCheck>();
+    }
+
+    public Vector3 pos 
+    {
+        get { return this.transform.position; }
+        set { this.transform.position = value; }
+    }
+    // Start is called before the first frame update
+    public virtual void Move()
+    {
+        Vector3 temPos = pos;
+        temPos.y-=speed*Time.deltaTime;
+        pos=temPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (!bndCheck.isOnScreen) 
+        {
+            if (pos.y < bndCheck.cameraHeight - bndCheck.radius) 
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
